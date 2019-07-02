5bd2df4486c69ceafd7fa55b1e85d443e00765bf
Nuno Peixoto @ Primavera BSS <nuno.peixoto@primaverabss.com>
2019-07-02 15:08:52 +0100

    PRIMAVERA Platform Extensibility Upgrader
    Small sample to add extensions to ERP v10 from an integration application

---
+++ Primavera.Platform.ExtensionsUpgrader/Primavera.Platform.ExtensionsUpgrader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Primavera.Platform.ExtensionsUpgrader
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += Helper.CurrentDomain_AssemblyResolve;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                    
+            Application.Run(new Manager());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString());
+        }
+    }
+}
