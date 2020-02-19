e9c9ba1d57cf83cbd456fdbb6d58823dc1034b50
danielafa9 <daniel_afa9@hotmail.com>
2020-02-19 12:52:50 +0000

    New class created to place code samples This class is a quick way to lear the API for performing various operations.

---
+++ Primavera.HumanResources/CodeExamples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primavera.Base.Party
+{
+    class CodeExamples
+    {
+        /// <summary>
+        /// Processings a specific list of employees.
+        /// </summary>
+        /// <param name="EmployeesList">The employees list.</param>
+        private void ProcessingEmployees(List<string> EmployeesList, int Year, int Month)
+        {
+            string periodType = PriEngine.Engine.RecursosHumanos.Params.PeriodoPorDefeito;
+            RhpBE100.RhpBELinhaPeriodo period = PriEngine.Engine.RecursosHumanos.Periodos.EditaPeriodoEspecifico(periodType, Month, Year);
+
+            string message = "";
+
+            foreach (string employeeNumber in EmployeesList)
+            {
+               PriEngine.Engine.RecursosHumanos.Processamento.EfectuaProcessamentoAutomatico(RhpBE100.TipoProc.tpVencimento, Year, Month, Month, periodType, period.DiasUteis, period.DataFim, period.DataFim, employeeNumber, ref message);
+            }
+        }
+
+    }
+}
